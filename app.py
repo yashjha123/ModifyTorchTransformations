125d8b696c39bdd9085dbf98f4f61ef4675d9123
Yash Jha <breakdownninja@gmail.com>
2022-11-09 05:00:04 +0530

    Scale and Normalise Features

--- app.py
+++ app.py
@@ -2,15 +2,23 @@ import flask
 from flask import request, jsonify
 import os
 
+from requests_toolbelt import MultipartEncoder
+
+
+
+import numpy as np
 import torch
 import torchvision
 
+import base64
+import cv2
 import uuid
 from flask_cors import CORS, cross_origin
 import PIL
 from PIL import Image
 import json
 
+import utils
 UPLOAD_FOLDER ="./folder/"
 
 app = flask.Flask(__name__)
@@ -24,8 +32,7 @@ class JsonDatabase():
         super().__init__()
         self.state = initialState
         self.loadState()
-    def saveState(self):
-        print(self.state)
+    def saveState(self):    
         with open('data.json', 'w') as f:
             json.dump(self.state, f)
     def loadState(self):
@@ -40,7 +47,6 @@ class JsonDatabase():
     def lookFor(self,id):
         self.loadState()
         for val in self.state:
-            print(val)
             if val["id"]==id:
                 return val
         return False
@@ -57,49 +63,68 @@ def get_id():
     val = database.add({"path":path})
     print((val))
     return jsonify(val)
-# import PIL
-# @app.route('/getImage'):
-#     return flask.send_file(
-#         output,
-#         mimetype='image/png',
-#         as_attachment=True,
-#         download_name='%s.png' % file_path["id"])
 
 @app.route('/applyTransformations', methods=["POST"])
 @cross_origin()
 def applyTransformations():
     print(request.is_json)
     obj = request.json
+    conf = obj["conf"]
     # print(request.form[''])
-    print()
+    print(conf)
     file_path = database.lookFor(obj["id"])
     img = Image.open(file_path["path"])
     print(obj["transforms"])
-    tts = []
-    exec("tts.append("+obj["transforms"]+")",{},{"tts":tts,"transforms":torchvision.transforms,'torchvision':torchvision,'torch':torch})
-    img = tts[0](img)
-#     imgFilePath = 
-    print(type(img)==PIL.PngImagePlugin.PngImageFile)
-    # img = torchvision.transforms.ToPILImage()(img)
-    # print(img)
-    # flask.make_response
-    global val123
-    val123 = io.BytesIO()
-    img.save(val123, format='PNG')
-    # img.save("img.png")
-    # print(val123.getvalue())
-    response = flask.make_response(val123.getvalue())
-    response.headers.set('Content-Type', 'image/png')
-    response.headers.set(
-        'Content-Disposition', 'attachment', filename='%s.png' % file_path["id"])
-    return response
-    # print(val123)
-    # return jsonify({"database":request.json["id"],'val':str(type(img))})
-    # return flask.send_file(
-    #     val123,
-    #     mimetype='image/png',
-    #     as_attachment=True,
-    #     download_name='%s.png' % file_path["id"])
+    custom = []
+    exec("tts.append("+obj["transforms"]+")",{},{"tts":custom,'torchvision':torchvision,"transforms":torchvision.transforms,'torch':torch})
+    img = custom[0](img)
+    file_type = ""
+    
+    buffer = io.BytesIO()
+    is_scaled, is_normalised = False, False
+    if type(img)==torch.Tensor:
+        file_type = "Tensor"
+        
+        if conf["scale_range"]: img, is_scaled = utils.scale_range(img)
+        if conf["normalize_range"]: img, is_normalised = utils.normalize_range(img)
+
+        img = img.numpy().astype(np.uint8)
+        img = np.moveaxis(img,0,-1)
+        img = Image.fromarray(img)
+        img.save(buffer, format='PNG')
+    elif type(img) == np.ndarray:
+        file_type = "Numpy"
+        
+        if conf["scale_range"]: img, is_scaled = utils.scale_range(img)
+        if conf["normalize_range"]: img, is_normalised = utils.normalize_range(img)
+
+        img = Image.fromarray(img)
+        img.save(buffer, format='PNG')
+    else:
+        file_type = "PIL Image"
+        img.save(buffer, format='PNG')
+    json_reponse = {"file_type":file_type,
+                    "file_type_encoded":str(type(img)),
+                    "is_scaled":is_scaled,
+                    "is_normalised":is_normalised}
+    # response = flask.make_response(buffer.getvalue())
+    # response.headers.set('Content-Type', 'image/png')
+    # response.headers.set('file_type',file_type)
+    # response.headers.set('file_type_encoded',str(type(img)))
+    # response.headers.set('is_scaled',is_scaled)
+    # response.headers.set('is_scaled',is_normalised)
+    # response.headers.set(
+    #     'Content-Disposition', 'attachment', filename='%s.png' % file_path["id"])
+    # return response
+    m = MultipartEncoder(
+        fields={
+            'field0':(None, json.dumps(json_reponse), "application/json"),
+            'field1':("image.png",buffer,"image/png")
+        }
+    )
+    return flask.Response(m.to_string(),mimetype=m.content_type)
+    # return jsonify(json_reponse)
+
 @app.route('/', methods=['GET', 'POST'])
 def home():
     return flask.render_template('./filename.html')
---
+++ utils.py
@@ -0,0 +1,21 @@
+import torch
+from PIL import Image
+import numpy as np
+def scale_range(img_array):
+    if img_array.max()<=1:
+        print("scaling image")
+        return img_array*255, True
+    return img_array, False
+def fit_array(img_array):
+    return np.moveaxis(img_array,0,-1)
+def normalize_range(img_array):
+    print(img_array.min())
+    # for layer in img_array:
+    img_array+=img_array.min()
+    img_array/=img_array.max()
+    img_array, _ = scale_range(img_array)
+    return img_array, True
+
+# a = torch.zeros((3,100,100))
+# a = torch.min(a,dim=0)
+# print(a)
